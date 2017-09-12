1dd0a6a6c65904500926a8e0e41fd3d96e4781e3
varunreddyp <varunreddy428@gmail.com>
2017-09-11 18:34:49 -1200

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,16 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
-	}
+	Scanner s=new Scanner(System.io);
+	System.out.println("enter student id");
+	int id=s.nextInt();
+	System.out.println("enter the student fullname");
+	string fname=s.next();
+	System.out.println("Enter the student date of birth");
+	strringdobirth=s.next();
+	Date dob=new SimpledateFormat("MM,dd,yyyy"Locale.ENGLISH).parse(dob);
+	System.out.println("the student average marks");
+		
+}
 
 }
